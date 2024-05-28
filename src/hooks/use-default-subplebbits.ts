0c18ec1b7e1cd81551846f57e41d4b1b1145daf2
ComfyPlebBro <145825073+ComfyPlebBro@users.noreply.github.com>
2024-05-27 21:28:42 -0700

    hello world

--- src/hooks/use-default-subplebbits.ts
+++ src/hooks/use-default-subplebbits.ts
@@ -40,7 +40,7 @@ export const useDefaultSubplebbits = () => {
     (async () => {
       try {
         const multisub = await fetch(
-          'https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/multisub.json',
+          'https://raw.githubusercontent.com/ComfyPlebBro/temporary-default-subplebbits/master/multisub.json',
           // { cache: 'no-cache' }
         ).then((res) => res.json());
 
